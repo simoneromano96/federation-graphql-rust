56a134038e830a3cb8e6270ef959c0b22ed00a31
Simono il magnifico <simoneromano@tutanota.de>
2020-11-07 22:06:31 +0100

    Fix Gateway

--- gateway/src/index.ts
+++ gateway/src/index.ts
@@ -1,82 +1,9 @@
-// Require the framework and instantiate it
 import fastify from "fastify"
 import mercurius from "mercurius"
-import { getGraphQLParameters, processRequest } from "graphql-helix"
-import { Request as GQLRequest } from "graphql-helix/dist/types"
-import fastifyWebsocket from "fastify-websocket"
 import { stitchSchemas } from "@graphql-tools/stitch"
 import { loadSchema } from "@graphql-tools/load"
+import WebSocket from "ws"
 import { UrlLoader } from "@graphql-tools/url-loader"
-
-// import { gatewaySchema } from "./graphql/schema"
-
-/*
-app.register(fastifyWebsocket)
-
-app.all("/graphql", async (request, response) => {
-  const gqlRequest = {
-    body: request.body,
-    headers: request.headers,
-    method: request.method,
-    query: request.query,
-  }
-
-  const { query, variables, operationName } = getGraphQLParameters(gqlRequest)
-
-  const result = await processRequest({
-    schema: gatewaySchema,
-    query,
-    variables,
-    operationName,
-    request,
-  })
-
-  if (result.type === "RESPONSE") {
-    const headers = new Map()
-    result.headers.forEach(({ name, value }) => headers.set(name, value))
-    response.headers(headers)
-    response.status(result.status)
-    response.send(result.payload)
-  } else {
-    // TODO
-  }
-})
-
-app.get("/ws", { websocket: true }, async (connection, request, params) => {
-  connection.socket.on("message", async (message: any) => {
-    app.log.info({ message })
-
-    const gqlRequest: GQLRequest = {
-      body: message,
-      headers: request.headers,
-      method: request.method as string,
-      query: undefined,
-      // query: request.url,
-    }
-
-    app.log.info({ gqlRequest })
-
-    const { query, variables, operationName } = getGraphQLParameters(gqlRequest)
-
-    app.log.info(query as string)
-    app.log.info(variables as string)
-    app.log.info(operationName as string)
-
-    const result = await processRequest({
-      schema: gatewaySchema,
-      query,
-      variables,
-      operationName,
-      request: { ...request, method: request.method as string, query: undefined },
-    })
-
-    app.log.info({ result })
-
-    // message === 'hi from client'
-    connection.socket.send("hi from server")
-  })
-})
-*/
 
 // Run the server!
 const main = async () => {
@@ -87,6 +14,7 @@ const main = async () => {
       // load from endpoint
       loaders: [new UrlLoader()],
       enableSubscriptions: true,
+      webSocketImpl: WebSocket,
     })
 
     console.log(schema1)
